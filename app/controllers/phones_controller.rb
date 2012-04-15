1fea3664a885c0b79c5381303fb062f0a57ae580
Patrick Turley [pturley] <patrick.turley@gmail.com>
2012-04-15 19:28:12 +0200

    Turley & Craig: Adding the phone CRUD

---
+++ app/controllers/phones_controller.rb
@@ -0,0 +1,43 @@
+class PhonesController < ApplicationController
+
+  def index
+    @phones = Phone.all
+  end
+
+  def new
+    @phone = Phone.new
+  end
+
+  def create
+    @phone = Phone.new params[:phone]
+    if @phone.save
+      redirect_to phones_path
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @phone = Phone.find params[:id]
+  end
+
+  def update
+    @phone = Phone.find params[:id]
+    if @phone.update_attributes params[:phone]
+      redirect_to phones_path
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @phone = Phone.find params[:id]
+    @phone.destroy
+    redirect_to phones_path
+  end
+
+  def show
+    @phone = Phone.find params[:id]
+  end
+
+end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,2 +1,3 @@
 class Phone < ActiveRecord::Base
+  attr_accessible :make
 end
