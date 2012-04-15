73ee6820310df1e2c48f782a356c95252eb6b4df
Patrick Turley [pturley] <patrick.turley@gmail.com>
2012-04-15 17:43:32 +0200

    Turley & Craig: Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Numb::Application.config.secret_token = '2b35f8edae5fd4f0ccfa118acbf0a7672fb60a89aa450842f4d583b9e44315d0f749dbcc192f2bf4f8ddc1c5eb27c5c4d40c278c4c96541052a19ce8adbc8539'
