1491f2080b538e8e2119368ce12788f8c17ab0cf
Patrick Turley [pturley] <patrick.turley@gmail.com>
2012-04-17 22:22:44 +0200

    Turley: trying some prod config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,12 +59,12 @@ Numb::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :log
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Default url for devise that may not be necessary for us
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'numb.heroku.com' }
 end
